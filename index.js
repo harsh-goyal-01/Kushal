01fc8d8702f5083be52e4010e927ef25bc207f60
harsh-goyal-01 <77243780+harsh-goyal-01@users.noreply.github.com>
2023-04-16 14:48:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const express=require('express');
+const app=express();
+if(process.env.NODE_ENV!=='production')
+{
+    require('dotenv').config();
+}
+app.set('view engine','ejs');
+
+app.get('/',(req,res)=>{
+    res.render("index");
+})
+app.get('/home',(req,res)=>{
+    res.render("index");
+})
+app.get('/products',(req,res)=>{
+    res.render("products");
+})
+app.get('/about',(req,res)=>{
+    res.render("about");
+});
+const port=process.env.PORT;
+app.listen(port,()=>{
+    console.log('server started');
+})
